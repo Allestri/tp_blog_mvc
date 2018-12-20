5722762c47143f9bba511108f87f7b670b2c0c05
SOku <alexandre.pellae@gmail.com>
2018-12-20 05:09:41 +0100

    Creation template

---
+++ template.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title><?= $title ?></title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	
+	<body>
+		<h1>Bienvenue sur mon Blog</h1>
+		<p>Derniers billets du blog</p>
+		<?= $content ?>
+	</body>
+</html>
