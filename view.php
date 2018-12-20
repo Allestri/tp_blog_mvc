9541db46cd937c88a90509766e2bbfa33300bfce
SOku <alexandre.pellae@gmail.com>
2018-12-20 05:14:24 +0100

    Modifié view.php en fonction du template crée

--- view.php
+++ view.php
@@ -1,34 +1,25 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Mon blog</title>
-        <link href="style.css" rel="stylesheet" /> 
-    </head>
-        
-    <body>
-        <h1>Mon super blog !</h1>
-        <p>Derniers billets du blog :</p>
- 
-        
-        <?php
-        while ($data = $posts->fetch())
-        {
-        ?>
-            <div class="billet">
-                <h3>
-                    <?php echo htmlspecialchars($data['title']) ?>
-                    <em>posté le <?php echo htmlspecialchars ($data['creation_date_fr']) ?></em>
-                </h3>
-                
-                <p>
-                    <?php echo(htmlspecialchars($data['content'])) ?>
-                </p>
-				<a href="post.php?postId=<?php echo $data['id']; ?>">Commentaires</a>
-            </div>
-        <?php
-        }
-        $posts->closeCursor();
-        ?>
-    </body>
-</html>
+<?php $title ="Mon blog"; ?>
+<?php ob_start(); ?>
+
+
+<?php
+    while ($data = $posts->fetch())
+    {
+    ?>
+	<div class="billet">
+		<h3>
+			<?php echo htmlspecialchars($data['title']) ?>
+			<em>posté le <?php echo htmlspecialchars ($data['creation_date_fr']) ?></em>
+		</h3>
+
+		<p>
+			<?php echo(htmlspecialchars($data['content'])) ?>
+		</p>
+		<a href="post.php?postId=<?php echo $data['id']; ?>">Commentaires</a>
+	</div>
+<?php
+}
+$posts->closeCursor();
+?>
+<?php $content = ob_get_clean();?>
+<?php require ('template.php'); ?>
