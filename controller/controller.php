1a78b96e1095bc9211265078ed0dff99b9a0cefe
SOku <alexandre.pellae@gmail.com>
2018-12-26 03:49:49 +0100

    Gestion erreurs avec les Exceptions

--- controller/controller.php
+++ controller/controller.php
@@ -25,7 +25,8 @@ function addComment($postId, $author, $comment)
 	$affectedLines = postComment($postId, $author, $comment);
 	
 	if ($affectedLines === false) {
-		die('Impossible d\'ajouter le commentaire!');
+		// Erreur gerée. Elle sera remntée jusqu'au bloc try du routeur !
+		throw new Exception('Impossible d\'ajouter le commentaire !');
 	}
 	else {
 		header('Location: index.php?action=post&id=' . $postId);
--- model/model.php
+++ model/model.php
@@ -2,15 +2,8 @@
 // Connexion BDD
 function dbConnect()
 {
-	try
-	{
-		$db = new PDO('mysql:host=localhost;dbname=tp_blog_mvc;charset=utf8', 'root', '');
-		return $db;
-	}
-	catch(Exception $e)
-	{
-			die('Erreur : '.$e->getMessage());
-	}
+	$db = new PDO('mysql:host=localhost;dbname=tp_blog_mvc;charset=utf8', 'root', '');
+	return $db;
 }
 
 function getPosts()
